82609ac94c893181a70b3228676ac38bf5b43dd5
Hitender Kumar <hitender@backbase.com>
2021-10-13 23:28:35 +0530

    Use 'ImageDetail' component in 'ImageScreen' and pass custom props from ImageScreen to ImageDetail

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import ImageDetail from "../components/ImageDetail";
 
 const ImageScreen = () => {
-    return <Text>Image Screen</Text>
+    return (
+        <View>
+            <ImageDetail title='Forest' imageSource={require('../../assets/forest.jpg')} score={9} />
+            <ImageDetail title='Beach' imageSource={require('../../assets/beach.jpg')} score={10} />
+            <ImageDetail title='Mountain' imageSource={require('../../assets/mountain.jpg')} score={11} />
+        </View>
+    );
 };
 
 const styles = () => { };
