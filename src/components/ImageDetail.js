3dfae4b645e7e3fbebd0debc0e268106c51a46e9
Hitender Kumar <hitender@backbase.com>
2021-10-15 13:08:38 +0530

    Design 'ImageDetail' view and put contents in scrollview in ImageScreen view

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -3,14 +3,35 @@ import { View, Text, StyleSheet, Image } from "react-native";
 
 const ImageDetail = ({ imageSource, title, score }) => {
     return (
-        <View>
-            <Image source={ imageSource } />
-            <Text>{ title }</Text>
-            <Text>Image score - { score }</Text>
+        <View style={styles.container} >
+            <Image style={styles.image} source={imageSource} />
+            <View style={{ margin: 10, backgroundColor: 'green', flex:1 }}>
+                <Text>{title}</Text>
+                <Text style={{ marginTop: 10 }}>Image score - {score}</Text>
+                <View style={{marginTop:10, backgroundColor:'yellow', flex:1, flexDirection: 'row', justifyContent: 'space-between', alignItems:'center' }}>
+                    <Image style={{ marginLeft:20, width: 30, height: 30, borderRadius: 15 }} source={imageSource} />
+                    <Image style={{ width: 30, height: 30, borderRadius: 15 }} source={imageSource} />
+                    <Image style={{ width: 30, height: 30, borderRadius: 15, marginRight: 20 }} source={imageSource} />
+                </View>
+            </View>
         </View>
     );
 };
 
-const styles = StyleSheet.create({ });
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'red',
+        height: 120,
+        flexDirection: 'row'
+    },
+
+    image: {
+        marginLeft: 10,
+        marginTop: 10,
+        backgroundColor: 'green',
+        width: '50%',
+        height: 100
+    }
+});
 
 export default ImageDetail;
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 import ImageDetail from "../components/ImageDetail";
 
 const ImageScreen = () => {
     return (
-        <View>
+        <ScrollView>
             <ImageDetail title='Forest' imageSource={require('../../assets/forest.jpg')} score={9} />
             <ImageDetail title='Beach' imageSource={require('../../assets/beach.jpg')} score={10} />
             <ImageDetail title='Mountain' imageSource={require('../../assets/mountain.jpg')} score={11} />
-        </View>
+            <ImageDetail title='Mountain' imageSource={require('../../assets/mountain.jpg')} score={11} />
+            <ImageDetail title='Mountain' imageSource={require('../../assets/mountain.jpg')} score={11} />
+            <ImageDetail title='Mountain' imageSource={require('../../assets/mountain.jpg')} score={11} />
+            <ImageDetail title='Mountain' imageSource={require('../../assets/mountain.jpg')} score={11} />
+            <ImageDetail title='Mountain' imageSource={require('../../assets/mountain.jpg')} score={11} />
+        </ScrollView>
     );
 };
 
