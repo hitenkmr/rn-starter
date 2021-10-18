eddbf721de0ea0caee467250822926e17e753188
Hitender Kumar <hitender@backbase.com>
2021-10-18 12:04:16 +0530

    replace position, top, bottom, left, right with single '...StyleSheet.absoluteFillObject'

--- src/Screen Layouts/LayoutsScreen.js
+++ src/Screen Layouts/LayoutsScreen.js
@@ -183,11 +183,7 @@ const styles = StyleSheet.create({
     box7text1Style: {
         borderWidth: 2,
         borderColor: 'yellow',
-        position:'absolute',
-        top:0,
-        bottom:0,
-        left:0,
-        right:0
+        ...StyleSheet.absoluteFillObject
     },
 
     box7text2Style: {
